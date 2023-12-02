78e217156b4fda212a8c38470228231fd746fb97
StefanPej <79942683+StefanPej@users.noreply.github.com>
2023-12-02 17:18:22 +1100

    only floss the teeth you want to keep

--- 2023/day_02/part_1.py
+++ 2023/day_02/part_1.py
@@ -1,22 +1,20 @@
 with open("input.txt", "r") as o:
     inputs = o.readlines()
 
-max_cubes = {'red': 12,
-             'green': 13,
-             'blue': 14}
+max_cubes = {"red": 12, "green": 13, "blue": 14}
+
 
 def get_game_no(line):
-    return int(line.split(':')[0].split(" ")[1])
+    return int(line.split(":")[0].split(" ")[1])
+
 
 def get_cube_showing(line):
-    showings = line.split(':')[1].split(';')
-    game_max = {'red': 0,
-             'green': 0,
-             'blue': 0}
+    showings = line.split(":")[1].split(";")
+    game_max = {"red": 0, "green": 0, "blue": 0}
     for showing in showings:
-        dice_shown = showing.split(',')
+        dice_shown = showing.split(",")
         for dice in dice_shown:
-            num, colour = dice.strip().split(' ')
+            num, colour = dice.strip().split(" ")
             game_max[colour] = max(game_max[colour], int(num))
 
     for k, v in game_max.items():
@@ -24,10 +22,10 @@ def get_cube_showing(line):
             return 0
 
     return get_game_no(line)
-    
+
+
 total = 0
 for line in inputs:
     total += get_cube_showing(line)
 
 print(total)
-
