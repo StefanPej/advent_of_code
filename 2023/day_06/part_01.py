6d9be89151af2d760d4490c7fc696402231fc639
Stefan Pejnovic <stefan.pejnovic@cba.com.au>
2023-12-06 16:10:05 +1100

    MAN SOMETIMES YOU JUST NEED A WIN AND THEN YOU GET IT AND YOU FEEL GOOD BUT THEN REMEMBER THAT YESTERDAY STILL ISNT SOLVED THEN YOU FEED BAD AGAIN

---
+++ 2023/day_06/part_01.py
@@ -0,0 +1,21 @@
+with open("./2023/day_06/input.txt", "r") as o:
+    input = o.readlines()
+
+times = input[0].split()[1:]
+times = [int(time) for time in times]
+dists = input[1].split()[1:]
+dists = [int(dist) for dist in dists]
+
+def how_many_ways_to_win(time, dist):
+    ret = 0
+    for hold in range(time):
+        time_left = time-hold
+        if time_left*hold > dist:
+            ret += 1
+    return ret
+
+ans = 1
+for time, dist in zip(times, dists):
+    ans *= how_many_ways_to_win(time, dist)
+
+print(ans)
