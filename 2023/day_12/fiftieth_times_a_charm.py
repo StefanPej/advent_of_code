8ccd97e65e12969880cbd8e94a5895084086bc1a
Stefan Pejnovic <stefan.pejnovic@cba.com.au>
2023-12-14 09:23:27 +1100

    for dib

--- 2023/day_12/fiftieth_times_a_charm.py
+++ 2023/day_12/fiftieth_times_a_charm.py
@@ -3,25 +3,26 @@ from functools import cache
 
 inp = read_input(2023, 12)
 
-with open("./2023/day_12/test.txt", "r") as o:
-    inp = o.readlines()
+# with open("./2023/day_12/test.txt", "r") as o:
+#     inp = o.readlines()
 
 springs = []
 for line in inp:
     spring, broken = line.split()
     temp_sp = []
     temp_br = []
-    for _ in range(5):
-        temp_sp.append(spring)
-        temp_br.append(broken)
-    spring = "?".join(sp for sp in temp_sp)
-    broken = ','.join(br for br in temp_br)
+    # for _ in range(5):
+    #     temp_sp.append(spring)
+    #     temp_br.append(broken)
+    # spring = "?".join(sp for sp in temp_sp)
+    # broken = ','.join(br for br in temp_br)
     broken = broken.split(',')
     broken = [int(broke) for broke in broken]
     springs.append(([*spring], broken))
 
 def check_valid(spring, groups):
     spring = ''.join(char for char in spring)
+    spring = spring.replace('?', '.')
     grouped_broken = spring.split('.')
     grouped_broken = [group for group in grouped_broken if group != '']
     if len(grouped_broken) != len(groups):
@@ -32,21 +33,39 @@ def check_valid(spring, groups):
     #print(spring)
     return 1
 
-@cache
-def solve(spring, groups):
-    #print(spring)
-    spring_str = "".join(char for char in spring)
-    spring = list(spring)
+def check_if_possible(spring, groups, left_to_place):
+    spring = ''.join(char for char in spring)
+    amt_q = spring.count('?')
+    amt_hash = spring.count('#')
+    spring = spring.replace('?', '.')
+    grouped_broken = spring.split('.')
+    grouped_broken = [group for group in grouped_broken if group != '']
+    if amt_q < left_to_place-amt_hash:
+        return 0
+    if len(grouped_broken) > len(groups):
+        #print('asdasd')
+        return 0
+    if max(groups) < len(max(grouped_broken, key=len)):
+        return 0
+    return 1
+    
 
-    for i in range(len(spring)):
+def solve(spring, groups, index, left_to_place):
+    
+    if left_to_place == 0:
+        return check_valid(spring, groups)
+    
+    # if not check_if_possible(spring, groups, left_to_place):
+    #     return 0
+
+    for i in range(index, len(spring)):
         if spring[i] == '?':
             spring[i] = '#'
             left = spring.copy()
             spring[i] = '.'
             right = spring.copy()
-            return solve(tuple(left), groups) + solve(tuple(right), groups)
-
-    return check_valid(spring_str, groups)
+            return solve(left, groups, i, left_to_place-1) + solve(right, groups, i, left_to_place)
+    return check_valid(spring, groups)
 
 cache = {}
     
@@ -56,6 +75,6 @@ cache = {}
 # #print(solve(test[0], test[1], 1))
     
 total = 0
-for spring in springs[5:6]:
-    total += solve(tuple(spring[0]), tuple(spring[1]))
+for spring in springs:
+    total += solve(spring[0], spring[1], 0, sum(spring[1]))
 print(total)
