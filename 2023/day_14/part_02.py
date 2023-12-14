b1dfb8fd04a33d6ee299b0745ee24bb2f30193ab
Stefan Pejnovic <stefan.pejnovic@cba.com.au>
2023-12-15 09:00:58 +1100

    cha_cha_slide now supports reverse operations

--- 2023/day_14/part_02.py
+++ 2023/day_14/part_02.py
@@ -2,53 +2,34 @@ from aoc_utils import *
 
 inp = read_input(2023, 14)
 
-def cha_cha_slide(row, reverse=False):
-    row = list(row)
-    if not reverse:
-        for i in range(len(row)):
-            if row[i] == 'O':
-                current_pos = i
-                next_pos = i-1
-                while current_pos > 0 and next_pos > -1:
-                    if row[next_pos] in ['0', '#']:
-                        break
-                    row[current_pos], row[next_pos] = row[next_pos], row[current_pos]
-                    current_pos -= 1
-                    next_pos -= 1
-    else:
-        for i in range(len(row)-1, -1, -1):
-            if row[i] == 'O':
-                current_pos = i
-                next_pos = i+1
-                while current_pos <len(row)-1 and next_pos < len(row):
-                    if row[next_pos] in ['0', '#']:
-                        break
-                    row[current_pos], row[next_pos] = row[next_pos], row[current_pos]
-                    current_pos += 1
-                    next_pos += 1
-        
-    return row
+def cha_cha_slide(row, reverse_row=False):
+    row = list(reversed(row)) if reverse_row else list(row)
+    
+    for i in range(len(row)):
+        if row[i] == 'O':
+            current_pos = i
+            next_pos = i-1
+            while current_pos > 0 and next_pos > -1:
+                if row[next_pos] in ['0', '#']:
+                    break
+                row[current_pos], row[next_pos] = row[next_pos], row[current_pos]
+                current_pos -= 1
+                next_pos -= 1
+
+    return list(reversed(row)) if reverse_row else row
 
 def run_cycle(inp):
-    north_rocks = []
-    for row in transpose(inp):
-        north_rocks.append(cha_cha_slide(row))
-    north_rocks = transpose(north_rocks)
-    
-    west_rocks = []
-    for row in north_rocks:
-        west_rocks.append(cha_cha_slide(row))
+    instructions = [(True, False), (False, False), (True, True), (False, True)]
+
+    for transpose_grid, reverse_row in instructions:
+        inp = transpose(inp) if transpose_grid else inp
+        rocks = []
+        for row in inp:
+            rocks.append(cha_cha_slide(row, reverse_row))
+        rocks = transpose(rocks) if transpose_grid else rocks
+        inp = rocks
         
-    south_rocks = []
-    for row in transpose(west_rocks):
-        south_rocks.append(cha_cha_slide(row, True))
-    south_rocks = transpose(south_rocks)
-        
-    east_rocks = []
-    for row in south_rocks:
-        east_rocks.append(cha_cha_slide(row, True))
-        
-    return east_rocks
+    return inp
         
     
 rocks = inp
