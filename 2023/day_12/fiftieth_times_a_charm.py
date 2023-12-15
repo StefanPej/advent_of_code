46fef043298cd7a4591bb708f8f0862f80b6dfcb
Stefan Pejnovic <stefan.pejnovic@cba.com.au>
2023-12-15 15:57:47 +1100

    hmm yes, maybe

--- 2023/day_12/fiftieth_times_a_charm.py
+++ 2023/day_12/fiftieth_times_a_charm.py
@@ -3,19 +3,19 @@ from functools import cache
 
 inp = read_input(2023, 12)
 
-with open("./2023/day_12/test.txt", "r") as o:
-    inp = o.readlines()
+# with open("./2023/day_12/test.txt", "r") as o:
+#     inp = o.readlines()
 
 springs = []
 for line in inp:
     spring, broken = line.split()
     temp_sp = []
     temp_br = []
-    for _ in range(5):
-        temp_sp.append(spring)
-        temp_br.append(broken)
-    spring = "?".join(sp for sp in temp_sp)
-    broken = ','.join(br for br in temp_br)
+    # for _ in range(5):
+    #     temp_sp.append(spring)
+    #     temp_br.append(broken)
+    # spring = "?".join(sp for sp in temp_sp)
+    # broken = ','.join(br for br in temp_br)
     broken = broken.split(',')
     broken = [int(broke) for broke in broken]
     springs.append(([*spring], broken))
@@ -66,18 +66,19 @@ def check_if_borked(spring, groups, index):
     
 
 def solve(spring, groups, index, left_to_place):
+    
+    key = (tuple(spring[index:]), left_to_place)
 
+    if not check_if_borked(spring, groups, index):
+        cache_[key] = 0
+        return cache_[key]
     
     if left_to_place == 0 or '?' not in spring:
-        cache_[(tuple(spring[index:]), left_to_place)] = check_valid(spring, groups)
-        return check_valid(spring, groups)
+        cache_[key] = check_valid(spring, groups)
+        return cache_[key]
     
-    if not check_if_borked(spring, groups, index):
-        cache_[(tuple(spring[index:]), left_to_place)] = 0
-        return 0
-    
-    if (tuple(spring[index:]), left_to_place) in cache_:
-        return cache_[(tuple(spring[index:]), left_to_place)]
+    if key in cache_:
+        return cache_[key]
 
     for i in range(index, len(spring)):
         if spring[i] == '?':
@@ -85,10 +86,12 @@ def solve(spring, groups, index, left_to_place):
             left = spring.copy()
             spring[i] = '.'
             right = spring.copy()
-            return solve(left, groups, i, left_to_place-1) + solve(right, groups, i, left_to_place)
+            key = (tuple(spring[index:]), left_to_place)
+            cache_[key] = solve(left, groups, i, left_to_place-1) + solve(right, groups, i, left_to_place)
+            return cache_[key]
         
-    cache_[(tuple(spring[index:]), left_to_place)] = check_valid(spring, groups)
-    return cache_[(tuple(spring[index:]), left_to_place)]
+    
+    return check_valid(spring, groups)
 
 cache_ = {}
     
