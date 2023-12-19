390a59c0051f8b0208fe6ea58337879f46b6af90
StefanPej <79942683+StefanPej@users.noreply.github.com>
2023-12-19 19:44:06 +1100

    smh my head no brute force

--- 2023/day_19/part_02.py
+++ 2023/day_19/part_02.py
@@ -4,6 +4,7 @@ inp = read_input(2023, 19)
 
 # with open('2023/day_19/test.txt') as o:
 #     inp = o.readlines()
+# inp = [line.strip() for line in inp]
 
 doing_workflows = True
 
@@ -27,90 +28,70 @@ for flow in workflows_l:
     inst_list = instructions.split(',')
     workflows[flow_name] = inst_list
 
-def run_instruction(part, inst):
-    if not ':' in inst:
-        return inst
-    instruction, loc = inst.split(':')
-    val = instruction[0]
-    operation = instruction[1]
-    comparitor = instruction[2:]
-
-    part_val = int(part[val])
-
-    if operation == '<':
-        if part_val < int(comparitor):
-            return loc
-    if operation == '>':
-        if part_val > int(comparitor):
-            return loc
-
-    return False
-
-parts = [part.replace('=',':') for part in parts]
-
-clean_parts = []
-for part in parts:
-    part = part.replace('{', '').replace('}', '')
-    part_l = part.split(',')
-    new_dict = {}
-    for l in part_l:
-        name, num = l.split(':')
-        new_dict[name] = num
-    clean_parts.append(new_dict)
-
-def brutus(range_tuple):
-    min_, max_ = range_tuple
-
-
-    total = 0
-    for i in range(min_,max_):
-        for j in range(min_,max_):
-            for k in range(min_,max_):
-                for l in range(min_,max_):
-                    part = {'x':i, 'm':j, 'a':k, 's':l}
-                    inst = 'in'
-                    while inst not in 'RA':
-                        instructions = workflows[inst]
-                        for ins in instructions:
-                            inst = run_instruction(part, ins)
-                            if inst:
-                                break
-                    if inst == 'A':
-                        total += 1
-                        #print(total)
-    return total
-
-# def brutus2(range_tuple):
-#     min_, max_ = range_tuple
-
-
-#     total = 0
-#     for i in range(0,2):
-#         for j in range(0,2):
-#             for k in range(0,2):
-#                 for l in range(0,4001):
-#                     part = {'x':i, 'm':j, 'a':k, 's':l}
-#                     inst = 'in'
-#                     while inst not in 'RA':
-#                         instructions = workflows[inst]
-#                         for ins in instructions:
-#                             inst = run_instruction(part, ins)
-#                             if inst:
-#                                 break
-#                     if inst == 'A':
-#                         total += 1
-#     return total
-
-from multiprocessing import Pool
-if __name__ == '__main__':
-    NUM_PROCS = 5
-    pool = Pool(NUM_PROCS)
-    ranges = [(1,800), (800, 1600), (1600,2400), (2400,3200), (3200,4001)]
-    rets = []
-    for res in pool.map(brutus, ranges):
-        rets.append(res)
-    print(rets)
-    print(sum(rets))
-
-
+def run_instruction(part, instruction):
+    if not ':' in instruction:
+        return [(instruction, part)]
     
+    comparison, dest = instruction.split(':')
+
+    letter_to_check = comparison[0]
+    operator = comparison[1]
+    number_to_check = int(comparison[2:])
+
+    part_min, part_max = part[letter_to_check]
+
+    if part_min <= number_to_check <= part_max:
+        if operator == '>':
+            new_range = (number_to_check+1, part_max)
+            continue_range = (part_min, number_to_check)
+        else:
+            new_range = (part_min, number_to_check-1)
+            continue_range = (number_to_check, part_max)
+        new_dict = part.copy()
+        continue_dict = part.copy()
+        new_dict[letter_to_check] = new_range
+        continue_dict[letter_to_check] = continue_range
+
+        return [(dest, new_dict), (False, continue_dict)]
+    
+    return [(False, part)]
+
+
+def run_workflow(part, flow):
+    ret = []
+    for inst in flow:
+        output = run_instruction(part, inst)
+        if len(output) == 2:
+            ret.append(output[0])
+            part = output[1][1]
+            out = output
+        else:
+            part = output[0][1]
+            out = output
+    if len(out) == 1:
+        ret.append(out[0])
+    return ret
+
+all_parts = {'x':(1,4000), 'm':(1,4000), 'a':(1,4000), 's':(1,4000)}
+
+accepted = []
+parts = [('in', all_parts)]
+while parts:
+    part = parts.pop(0)
+    flow, part_dic = part
+    out = run_workflow(part_dic, workflows[flow])
+    for x in out:
+        if x[0] == 'A':
+            accepted.append(x)
+        elif x[0] != 'R':
+            parts.append(x)
+
+total = 0
+for accept in accepted:
+    acc_dict = accept[1]
+    sub_val = 1
+    for v in acc_dict.values():
+        sub_val *= v[1] - v[0] +1
+    total += sub_val
+
+print(total)
