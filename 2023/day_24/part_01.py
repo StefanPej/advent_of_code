5037c6687724a9a26d0d812d9b895508fad37322
StefanPej <79942683+StefanPej@users.noreply.github.com>
2023-12-25 07:59:25 +1100

    haha needless computations in the inner loop!

--- 2023/day_24/part_01.py
+++ 2023/day_24/part_01.py
@@ -24,20 +24,19 @@ time = 10000000000000000000000
 count = 0
 
 for i, stone1 in enumerate(hail[:-1]):
+    s1_x, s1_y, _ = stone1[0]
+    s1_xv, s1_yv, _ = stone1[1]
+    stone1_line = LineString([(s1_x, s1_y), (s1_x + (time*s1_xv), s1_y + (time*s1_yv))])
+
     for stone2 in hail[i+1:]:
-        s1_x, s1_y, _ = stone1[0]
-        s1_xv, s1_yv, _ = stone1[1]
         s2_x, s2_y, _ = stone2[0]
         s2_xv, s2_yv, _ = stone2[1]
-        stone1_line = LineString([(s1_x, s1_y), (s1_x + (time*s1_xv), s1_y + (time*s1_yv))])
         stone2_line = LineString([(s2_x, s2_y), (s2_x + (time*s2_xv), s2_y + (time*s2_yv))])
 
         intersect = stone1_line.intersection(stone2_line)
-
         if intersect:
             x_int = intersect.xy[0][0]
             y_int = intersect.xy[1][0]
-
             if test_start <= x_int <= test_end and test_start <= y_int <= test_end:
                 count += 1
 
