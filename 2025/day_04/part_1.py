03c6cb194c8fe34888c354b918e939f252b2e688
StefanPej <79942683+StefanPej@users.noreply.github.com>
2025-12-05 07:29:32 +1100

    day 4

---
+++ 2025/day_04/part_1.py
@@ -0,0 +1,33 @@
+from aoc_utils import *
+
+inp = read_input(2025, 4)
+test = read_input(2025, 4, filename="test")
+
+
+def scan_around(row, col, grid):
+
+    row = int(row)
+    col = int(col)
+
+    max_rows = len(grid)-1
+    max_cols = len(grid[0])-1
+
+    around = [(row + nr, col + nc) for nr in range(-1,2,1) for nc in range(-1,2,1)]
+
+    count = 0
+    for nr, nc in around:
+        if (nr == row and nc == col) or (nc < 0) or (nc > max_cols) or (nr < 0) or (nr > max_rows):
+            continue
+        if grid[nr][nc] == "@":
+            count +=1
+
+    return count < 4
+
+count = 0
+for row_num, row in enumerate(inp):
+    for col_num, char in enumerate(row):
+        if char != "@":
+            continue
+        count += scan_around(row_num, col_num, inp)
+
+print(count)
