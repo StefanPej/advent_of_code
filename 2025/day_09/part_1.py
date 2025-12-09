45a8d47724ce8915c404d7bcbcba04307038fba3
StefanPej <79942683+StefanPej@users.noreply.github.com>
2025-12-10 06:54:35 +1100

    shapely question

--- 2025/day_09/part_1.py
+++ 2025/day_09/part_1.py
@@ -2,3 +2,13 @@ from aoc_utils import *
 
 inp = read_input(2025, 9)
 test = read_input(2025, 9, filename='test')
+
+rec = 0
+for i in range(len(inp)-1):
+    xi, yi = map(int, inp[i].split(","))
+    for j in range(i+1, len(inp)):
+        xj, yj = map(int, inp[j].split(","))
+        x_diff = abs(xi-xj) + 1
+        y_diff = abs(yi-yj) + 1
+        rec = max(x_diff*y_diff, rec)
+print(rec)
--- 2025/day_09/part_2.py
+++ 2025/day_09/part_2.py
@@ -1,4 +1,24 @@
 from aoc_utils import *
+from shapely import Polygon
 
 inp = read_input(2025, 9)
 test = read_input(2025, 9, filename='test')
+
+points=[]
+for line in inp:
+    points.append(tuple(map(int, line.split(","))))
+big = Polygon(points)
+
+rec = 0
+for i in range(len(points)-1):
+    xi, yi = points[i]
+    for j in range(i+1, len(points)):
+        xj, yj = points[j]
+        corners = [(xi, yi), (xi, yj), (xj, yj), (xj, yi)]
+        little = Polygon(corners)
+        if big.contains(little):
+            x_diff = abs(xi-xj) + 1
+            y_diff = abs(yi-yj) + 1
+            rec = max(x_diff*y_diff, rec)
+
+print(rec)
